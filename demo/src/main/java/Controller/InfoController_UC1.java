8c6b546b459e99f27f52ce598eb3dc04bc5fff92
StromKang <maxsore0425@gmail.com>
2021-04-14 01:25:35 +0900

    추가 설명

--- demo/src/main/java/Controller/InfoController_UC1.java
+++ demo/src/main/java/Controller/InfoController_UC1.java
@@ -16,7 +16,9 @@ import Security.Bcrypt;
 public class InfoController_UC1 {
 
 	public static final ModelAndView ERROR = new ModelAndView("JSP/Error.jsp");
-	
+	//중복 로그인 등으로 인해 소멸된 세션의 유저가 
+	// ~~.do 등의 데이터를 변경하려는 시도가 감지될때
+	// 유도되는 웹페이지
 	@Autowired
 	Bcrypt Bcry;
 	
@@ -29,6 +31,9 @@ public class InfoController_UC1 {
 		if(session.getAttribute("User_Num") == null) {
 			return ERROR;
 		}
+		// 소멸된 세션의 유저인지 검사 
+		// 첫 로그인때 무조건 Session 의 "User_Num" Attribute에 값이 등록되기 때문에 비교대상으로 적합
+		// 세션이 소멸되면 "User_Num" Attribute 값은 자동으로 Null 상태가 됌.
 		
 		ModelAndView mav = new ModelAndView("JSP/Modify_Info.jsp");
 		
